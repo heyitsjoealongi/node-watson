13e6540cba0ba6413674a639708d57e9173d1bc6
Joe Alongi <collectedview@gmail.com>
2021-07-31 02:07:19 -0400

    Route Session

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 
 app.use(express.json());
 
+const watsonRoutes = require("./routes/api/watson");
+
+app.use("/api/watson", watsonRoutes);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server listening on port", port);
