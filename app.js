8cdafaccfed42853b2004c6390bf1e4ec8b56eaf
Joe Alongi <collectedview@gmail.com>
2021-07-31 01:04:32 -0400

    Watson Pre-Integration

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello from Express!</h1>");
+const bodyParser = require("body-parser"),
+  DEFAULT_BODY_SIZE_LIMIT = 1024 * 1024 * 10,
+  DEFAULT_PARAMETER_LIMIT = 10000;
+
+const bodyParserJsonConfig = () => ({
+  parameterLimit: DEFAULT_PARAMETER_LIMIT,
+  limit: DEFAULT_BODY_SIZE_LIMIT,
 });
+
+const ask = require("./controller").ask;
+
+app.use(bodyParser.json(bodyParserJsonConfig()));
+
+app.get("/", (req, res) => res.send("<h1>Hello from Watson!</h1>"));
+app.post("/ask", ask);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
