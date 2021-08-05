e47f7b43abd7d9b3841573a9096d1345cdd1677c
Joe Alongi <collectedview@gmail.com>
2021-08-04 21:20:12 -0400

    Setup Redux

--- react-watson/src/App.js
+++ react-watson/src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello World!</div>;
-}
+import { Provider } from "react-redux";
+import store from "./store";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div className="App">Hello World!</div>
+    </Provider>
+  );
+};
 
 export default App;
