d40fc713324c5d57afd33861ffdce6ddc9a45946
Joe Alongi <collectedview@gmail.com>
2021-08-05 00:51:50 -0400

    Finish Styling & Add Streaming Effects

--- react-watson/src/components/Chat.js
+++ react-watson/src/components/Chat.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { connect } from "react-redux";
 
 import { userMessage, sendMessage } from "../actions/queue";
 
 const Chat = ({ chat, userMessage, sendMessage }) => {
   const [message, setMessage] = useState("");
+  const endOfMessages = useRef(null);
+
+  const scrollToBottom = () => {
+    endOfMessages.current.scrollIntoView({ behavior: "smooth" });
+  };
+  useEffect(scrollToBottom, [chat]);
 
   const handleClick = async (event) => {
     const code = event.keyCode || event.which;
@@ -16,20 +22,25 @@ const Chat = ({ chat, userMessage, sendMessage }) => {
       setMessage("");
     }
   };
+
   return (
     <>
-      <section className="chat">
-        <h1>Watson Chatbot</h1>
-        {chat.length === 0
-          ? ""
-          : chat.map((message) => (
-              <article className="{msg.type}">{message.message}</article>
-            ))}
+      <section className="chat-box">
+        <article className="chat-stream">
+          <h1>Watson Chatbot</h1>
+          {chat.length === 0
+            ? ""
+            : chat.map((message) => (
+                <article className="{msg.type}">{message.message}</article>
+              ))}
+          <div ref={endOfMessages}></div>
+        </article>
         <input
-          id="chatBox"
+          className="chat-form"
           onChange={(event) => setMessage(event.target.value)}
           onKeyPress={handleClick}
           value={message}
+          placeholder="Enter Your Message"
         ></input>
       </section>
     </>
