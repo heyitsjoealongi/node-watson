6ae640f37266a856f2e46f5ad6fa1f50b3875077
Joe Alongi <collectedview@gmail.com>
2021-08-05 23:29:58 -0400

    Node OR Port Changed
    Match React proxy

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const watsonRoutes = require("./watson");
 
 app.use("/", watsonRoutes);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3030;
 app.listen(port, () => {
   console.log("Server listening on port", port);
 });
