0fccd549b7e61c05852890e49e631f47462ae07a
Joe Alongi <collectedview@gmail.com>
2021-08-05 22:46:35 -0400

    Messaging Class & CSS

--- react-watson/src/components/Chat.js
+++ react-watson/src/components/Chat.js
@@ -25,23 +25,25 @@ const Chat = ({ chat, userMessage, sendMessage }) => {
 
   return (
     <>
-      <section className="chat-box">
-        <article className="chat-stream">
-          <h1>Watson Chatbot</h1>
-          {chat.length === 0
-            ? ""
-            : chat.map((message) => (
-                <article className="{msg.type}">{message.message}</article>
-              ))}
-          <div ref={endOfMessages}></div>
+      <section className="chat-session">
+        <article className="chat-box">
+          <article className="chat-stream">
+            <h1>Watson Chatbot</h1>
+            {chat.length === 0
+              ? ""
+              : chat.map((message) => (
+                  <article className={message.type}>{message.message}</article>
+                ))}
+            <div ref={endOfMessages}></div>
+          </article>
+          <input
+            className="chat-form"
+            onChange={(event) => setMessage(event.target.value)}
+            onKeyPress={handleClick}
+            value={message}
+            placeholder="Enter Your Message"
+          ></input>
         </article>
-        <input
-          className="chat-form"
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyPress={handleClick}
-          value={message}
-          placeholder="Enter Your Message"
-        ></input>
       </section>
     </>
   );
