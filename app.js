b86268a3cb3f4c7eb2cee5d4d7e6419a0a63a591
Joe Alongi <collectedview@gmail.com>
2021-08-07 18:31:13 -0400

    Add Node Path & Express Routes
    Direct to build directory for React files.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const app = express();
 
@@ -9,7 +11,13 @@ const watsonRoutes = require("./watson");
 
 app.use("/", watsonRoutes);
 
-const port = process.env.PORT || 3001;
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "react-watson", "build", "index.html"));
+});
+
+const port = process.env.PORT || 3030;
 app.listen(port, () => {
   console.log("Server listening on port", port);
 });
