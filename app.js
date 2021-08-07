903abf7ad0e74e866cc5fda5db3517b9fde6382e
Joe Alongi <collectedview@gmail.com>
2021-08-07 18:37:11 -0400

    Express Config Adjustments

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use("/", watsonRoutes);
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "react-watson", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 const port = process.env.PORT || 3030;
